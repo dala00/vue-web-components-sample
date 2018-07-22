d7fff7a64a583953f1118477b2a7761165eed11e
Shoichi Namba <alphabrend@gmail.com>
2018-07-22 14:20:38 +0900

    Page作成

--- src/Panel.js
+++
@@ -1,9 +0,0 @@
-export default class Panel {
-  constructor(index, x, y) {
-    this.index = index;
-    this.backgroundLeft = -75 * (index % 4);
-    this.backgroundTop = -75 * Math.floor(index / 4);
-    this.x = x;
-    this.y = y;
-  }
-}
--- src/main.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import wrap from '@vue/web-component-wrapper'
-import PuzzleButton from './components/PuzzleButton.vue?shadow'
-
-window.customElements.define('puzzle-button', wrap(Vue, PuzzleButton));
